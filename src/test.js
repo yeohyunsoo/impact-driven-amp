907201ef3102470e139dc7ebaf978f70bd22a3ce
여현수 <okestro@yeohyeonsuui-MacBookPro.local>
2025-02-06 21:38:14 +0900

    fetch/update

--- src/test.js
+++ src/test.js
@@ -75,8 +75,10 @@ const arrowFunction = (a, b) => a + b;
 console.log(arrowFunction(1, 2));
 
 // Hands On 6. function with anonymous function
-setTimeout(function(){
-    console.log("resting for 2 seconds");
+console.log("resting for 2 seconds, 아래 함수가 먼저 실행돼요!");
+
+setTimeout(function(){ //setTimeOut = asyncronous function; 일단 저 아래의 Hello, World! 부터 실행됨 ㅇㅇ\
+    console.log("this is how you use anonymous function inside another function");
 },2000)
 
 let simpleAnonymousFunction = () => {
@@ -85,15 +87,49 @@ let simpleAnonymousFunction = () => {
 
 simpleAnonymousFunction();
 
-
-
-
-
 // Hands On 7. function with rest parameter
+// - 'rest'라는 워딩에서 그대로 드러나듯, 정해지지 않은 개수의 나머지 param을 다룰 때 활용한다.
+// - 이때 나머지 param은 배열의 형태로 저장된다.
+
+function restParamFunctionWithNumber(...numbers){
+    return numbers.reduce((total, num) => total + num, 0);
+}
+
+console.log("restParamFunctionWithNumber: ", restParamFunctionWithNumber(1,2,3,4));
+
+
+function restParamFunctionWithString(greet, ...names){
+    // console.log(greet)
+    // names.map(i => console.log(i))
+    return(`${greet}, ${names.join(' and ')}`);
+}
+
+console.log("restParamFunctionWithString: ", restParamFunctionWithString('Hello', 'Alice', 'Bob', 'Charlie'));
 
 // Hands On 8. function with destructuring
+function destructuringFunction(a, b, c){
+    console.log(a, b, c);
+}
+
+destructuringFunction(1, 2, 3);
+
 
 // Hands On 9. function with spread operator
+function displayUserInfo({ name, age, country = "Unknown" }) {
+    console.log(`Name: ${name}`);
+    console.log(`Age: ${age}`);
+    console.log(`Country: ${country}`);
+} 
+  
+const userA = {
+    name: "Alice",
+    age: 25,
+    country: "USA"
+};
+  
+displayUserInfo(userA);
+displayUserInfo({ name: "Bob", age: 30 });
+  
 
 // Hands On 10. function with async & await
 
