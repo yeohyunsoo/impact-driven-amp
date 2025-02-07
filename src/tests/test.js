3d2576b01056f094d3f5d560ed890b0041db7010
여현수 <okestro@yeohyeonsuui-MacBookPro.local>
2025-02-07 11:53:25 +0900

    25.02.07/morning

--- src/tests/test.js
+++ src/tests/test.js
@@ -76,8 +76,8 @@ console.log(arrowFunction(1, 2));
 // Hands On 6. function with anonymous function
 console.log('resting for 2 seconds, 아래 함수가 먼저 실행돼요!');
 
-setTimeout(function () {
-  //setTimeOut = asyncronous function; 일단 저 아래의 Hello, World! 부터 실행됨 ㅇㅇ\
+setTimeout(function () { // 이게 비동기 함수임. () => {}의 arrow function으로도 만들 수 있을듯.
+  //setTimeOut = asyncronous function; 일단 저 아래의 Hello, World! 부터 실행됨
   console.log('this is how you use anonymous function inside another function');
 }, 2000);
 
@@ -135,8 +135,57 @@ displayUserInfo(userA);
 displayUserInfo({ name: 'Bob', age: 30 });
 
 // Hands On 10. function with async & await
+// 1. 프로그램 세상에는 두 종류의 작업이 있음 (동기, 비동기) >> 동기는 즉시 실행, 완료되는 작업인 반면 비동기는 시간이 걸리는 작업
+// 2. 자바스크립트는 싱글스레드 언어이지만, 시간이 오래 걸리는 작업은 이후 작업이 먼저 실행되게 함으로써 병렬처럼 보이게 할 수 있음 (멈추지 않고 계속 진행)
+// 3. BUT 때로는 비동기 작업이 완료된 후에만 다음 작업을 실행해야 할 때가 있음 (i.e, 데이터 가져온 다음에 그 데이터 기반으로 다음 작업 실행한다던가)
+// 4**. 비동기 함수의 역할은 비동기 작업이 완료될 때까지 코드 실행을 일시 중지 하도록 도와주는 것.
+
+// [AS-WAS] 옛날옛적에는 fecth와 .then() / .catch() 메서드를 사용해 비동기 작업을 처리했음
+// 1. 비동기 작업을 처리하는 함수를 만들고, 그 함수를 .then() 메서드에 전달
+// 2. 비동기 작업이 완료되면, .then() 메서드에 전달된 함수가 실행됨
+// 3. 비동기 작업이 실패하면, .catch() 메서드에 전달된 함수가 실행됨
+function fetchUserDataDeprecated() {
+  fetch('https://api.example.com/user')
+    .then(response => {
+      if (!response.ok) {
+        console.log('여기가 문제임 1'); // 디버깅 할 때 어디가 문제인지 찾는 유효한 방법ㅋㅋ
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Parse the JSON from the response
+    })
+    .then(data => {
+      console.log('User Name:', data.name); // Log the user's name
+    })
+    .catch(error => {
+      console.log('여기가 문제임 2'); // for debugging (나중에 자주 씀 ㅇㅇ)
+      console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+fetchUserDataDeprecated();
+
+// [AS-IS] 콜백 지옥 및 코드 가독성 저하 문제 발생 -> async, await 사용함
+// 1. async를 사용해 비동기 함수를 선언하고,
+// 2. 기다려야 하는 지점에서 await 키워드를 사용해 비동기 작업이 완료될 때까지 기다림
+
+async function fectchUserDataAsync(){
+  try {
+    const response = await fetch('https://api.example.com/user');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json(); // Parse the JSON from the response
+    console.log('User Name:', data.name); // Log the user's name
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
+fectchUserDataAsync();
 
 // Hands On 11. function with promise
+
+
 
 // Hands On 12. function with callback
 
