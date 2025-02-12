14492045adc95458d5685284de8872310d8d4f6f
dev-y <dugustn2002@gmail.com>
2025-02-12 23:20:40 +0900

    fix/package version fix

--- src/NewApp.jsx
+++ src/NewApp.jsx
@@ -4,9 +4,9 @@ import InputBox from "./components/InputBox";
 
 export default function NewApp(){ //react-심화 개념은 여기에서 다룸 ㅇㅇ
     return(
-        <div>
-            <h1>Okestro Knowledge Sharing System</h1>
-            <InputBox />
-        </div>
+        <Routes>
+            <Route path="/" element={<InputBox/>} />
+            <Route path="/content" element={<InputBox/>} />
+        </Routes>   
     )
 }
