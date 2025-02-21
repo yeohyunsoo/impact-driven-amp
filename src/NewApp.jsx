ff486d6c0ece76b2caeb8ba289cd96b837287483
dev-y <dugustn2002@gmail.com>
2025-02-21 23:11:02 +0900

    25.02.21/wrap-up basic hooks

--- src/NewApp.jsx
+++ src/NewApp.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import InputBox from "./components/InputBox";
 import DefaultFetchHookComponent from "./components/DefaultFetchHookComponent";
 import UseEffectTester from "./components/UseEffectTester";
+import UseEffectTesterTwo from "./components/UseEffectTesterTwo";
+
 export default function NewApp(){ //react-심화 개념은 여기에서 다룸 ㅇㅇ
     return(
         <Router>
@@ -11,6 +13,7 @@ export default function NewApp(){ //react-심화 개념은 여기에서 다룸 �
                 <Route path="/content" element={<InputBox/>} />
                 <Route path="/default-fetch-hook" element={<DefaultFetchHookComponent/>} />
                 <Route path="/use-effect-tester" element={<UseEffectTester/>} />
+                <Route path="/use-effect-tester-two" element={<UseEffectTesterTwo/>} />
             </Routes> 
         </Router>
     ) 
---
+++ src/components/UseEffectTesterTwo.jsx
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react';
+import useEffectOne from '../hooks/lifeCycleHandsOn/useEffectOne';
+
+export default function UseEffectTesterTwo() {
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    
+    useEffect(() => {
+        if (name && age) { //if both name and age are truthy
+            console.log(`Effect triggered with name: ${name} and age: ${age}`);
+            // Your effect logic here
+        }
+    }, [name, age]); //state change가 없으면 실행되지 않음 ㅇㅇ && 모든 state 변경에 대해 useEffect하고 싶지만, mount 됐을 때는 실행하지 않고 싶다면
+
+    const handleSubmit = () => {
+        const nameValue = document.querySelector('.name-input').value;
+        const ageValue = document.querySelector('.age-input').value;
+        
+        setName(nameValue);
+        setAge(ageValue);
+
+        console.log(`이름: ${nameValue} && 나이: ${ageValue}`);
+        
+        //원칙적으로 hook function은 콜백 함수 내에서의 사용이 불가함
+        
+    }
+
+    return (
+        <>
+            <input
+            className="name-input"    
+            type="text"
+            placeholder="이름을 입력하세요"
+            />
+            <input
+                className="age-input"    
+                type="number"
+                placeholder="나이를 입력하세요"
+            />
+            <button onClick={handleSubmit}>제출하기</button>
+        </>
+    )
+}
